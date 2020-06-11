1d7486b87717d6ae5233a8363ba7b8e45f4729f8
Raul Sanchez Rico <RSR@MacBook-Pro-de-Raul.local>
2020-06-11 03:09:37 -0500

    Tarea 2.2

--- T2.2/EcoUDPB/CEcoUDPB.java
+++ T2.2/EcoUDPB/CEcoUDPB.java
@@ -7,14 +7,48 @@ public class CEcoUDPB {
             System.out.print("Cliente iniciado, escriba un mensaje de saludo:");
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             String mensaje = br.readLine();
-            byte[] b = mensaje.getBytes();
+            int numPaquetes = (int)Math.ceil(mensaje.length()/20)+1;
+            String numPaquetesS = Integer.toString(numPaquetes);
+            System.out.println("NumPaquetes: "+numPaquetes+"\n"+"NumPaquetes S: "+numPaquetesS+"\n");
+
+            byte[] numPaquetesB = numPaquetesS.getBytes();
             String dst = "127.0.0.1";
-            int pto = 2000;
-            DatagramPacket p = new DatagramPacket(b,b.length, InetAddress.getByName(dst),pto);
-            cl.send(p);
+            int pto = 2019;
+            DatagramPacket numPaquetesP = new DatagramPacket(numPaquetesB,numPaquetesB.length, InetAddress.getByName(dst),pto);
+            cl.send(numPaquetesP);
+
+            int inicio = 0;
+            int finals = 19;
+            for(int i = 0; i < numPaquetes;i++){
+                if(i == numPaquetes-1){
+                    String subPaquete = mensaje.substring(inicio-1,mensaje.length());
+                    byte[] numPaquetesB1 = subPaquete.getBytes();
+                    String dst1 = "127.0.0.1";
+                    DatagramPacket numPaquetesP1 = new DatagramPacket(numPaquetesB1,numPaquetesB1.length, InetAddress.getByName(dst1),pto);
+                    cl.send(numPaquetesP1);
+                    break;
+                }
+                else{
+                    String subPaquete = mensaje.substring(inicio,finals);
+                    byte[] numPaquetesB2 = subPaquete.getBytes();
+                    String dst2 = "127.0.0.1";
+                    DatagramPacket numPaquetesP2 = new DatagramPacket(numPaquetesB2,numPaquetesB2.length, InetAddress.getByName(dst2),pto);
+                    cl.send(numPaquetesP2);
+                    inicio +=20;
+                    finals += 20;
+                }
+            }
             cl.close();
+            // byte[] b = mensaje.getBytes();
+            // String dst = "127.0.0.1";
+            // int pto = 2014;
+            // DatagramPacket p = new DatagramPacket(b,b.length, InetAddress.getByName(dst),pto);
+            // cl.send(p);
+            // cl.close();
         }catch(Exception e){
             e.printStackTrace();
         }//catch
     }//main
 }
+
+//Dividimos el mensaje entre 20
--- T2.2/EcoUDPB/SEcoUDPB.java
+++ T2.2/EcoUDPB/SEcoUDPB.java
@@ -4,14 +4,29 @@ import java.io.*;
 public class SEcoUDPB {
     public static void main(String[] args){
         try{
-            DatagramSocket s = new DatagramSocket(2000);
+
+            DatagramSocket s = new DatagramSocket(2019);
             System.out.println("Servidor iniciado, esperando cliente");
             for(;;){
+                // DatagramPacket p = new DatagramPacket(new byte[2000],2000);
+                // s.receive(p);
+                // System.out.println("Datagrama recibido desde" + p.getAddress()+":"+p.getPort());
+                // String msj = new String(p.getData(),0,p.getLength());
+                // System.out.println("Con el mensaje:"+ msj);
+                String mensajeCompleto = "";
                 DatagramPacket p = new DatagramPacket(new byte[2000],2000);
                 s.receive(p);
                 System.out.println("Datagrama recibido desde" + p.getAddress()+":"+p.getPort());
                 String msj = new String(p.getData(),0,p.getLength());
-                System.out.println("Con el mensaje:"+ msj);
+                System.out.println("Numero de paquetes"+ msj);
+                int numPaquetes = Integer.parseInt(msj);  
+                for(int i = 0;i < numPaquetes;i++){
+                    DatagramPacket p1 = new DatagramPacket(new byte[2000],2000);
+                    s.receive(p1);
+                    String msjPaquete = new String(p1.getData(),0,p1.getLength());
+                    mensajeCompleto += msjPaquete;
+                }
+                System.out.println("Mensaje Completo: "+ mensajeCompleto);
             }//for
             //s.close()
         }catch(Exception e){
